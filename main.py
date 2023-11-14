922d39b75dfa3484bca3f86e25792427bfaf0fa0
DavuKX <103606165+DavuKX@users.noreply.github.com>
2023-11-14 14:11:55 -0500

    set up validation history

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+from repositories.validated_words_repository import ValidatedWordsRepository
+from database.schemas.validated_word import ValidatedWordCreate
+from database.database import engine
+from database.models.validated_word import Base
+from database.schemas import validated_word
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
@@ -8,6 +14,14 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/validated_words/{uuid}")
+async def read_item(uuid: str):
+    repository = ValidatedWordsRepository()
+    by_uuid = repository.get_by_uuid(uuid)
+    return by_uuid
+
+
+@app.post("/validated_words/")
+async def create_item(validated_word: ValidatedWordCreate) -> validated_word.ValidatedWord:
+    repository = ValidatedWordsRepository()
+    return repository.create(validated_word)
